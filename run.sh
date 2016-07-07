9655ba318bc994a1b6301d3035ee8f6554456aad
HKLCF <hklcf@users.noreply.github.com>
2016-07-07 16:17:25 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-PATH=finished
+PROCESS_PATH=processing
+OUTPUT_PATH=finished
 
-if [ ! -e $PATH ]; then
-  /bin/mkdir $PATH
+if [ ! -e $PROCESS_PATH ]; then
+  /bin/mkdir $PROCESS_PATH
+fi
+
+if [ ! -e $OUTPUT_PATH ]; then
+  /bin/mkdir $OUTPUT_PATH
 fi
 
 for i in *.mkv
 do
+  /bin/mv $i $PROCESS_PATH/$i
   output="${i/mkv/mp4}"
-  ./ffmpeg -i $i -vf subtitles=$i $PATH/$output
-  /bin/mv $i $PATH/$i
+  ./ffmpeg -i $PROCESS_PATH/$i -vf subtitles=$PROCESS_PATH/$i $OUTPUT_PATH/$output
+  /bin/mv $PROCESS_PATH/$i $OUTPUT_PATH/$i
 done

0d6c94d04d828c21b369bdf3714bbccb2d3ced32
HKLCF <hklcf@users.noreply.github.com>
2016-08-29 11:22:56 +0800

    bug fix

--- run.sh
+++ run.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
-while [[ $# -gt 1 ]]
+while [[ $# -gt 0 ]]
 do
     key="$1"
     case $key in
         -s|--size)
-        SIZE="$2"
-        shift # past argument
-        ;;
+            SIZE="$2"
+            shift # past argument
+            ;;
         -o|--output)
-        OUTPUT="$2"
-        shift # past argument
-        ;;
+            OUTPUT="$2"
+            shift # past argument
+            ;;
         --debug)
-        DEBUG=YES
-        shift # past argument
-        ;;
+            DEBUG=YES
+            shift # past argument
+            ;;
         *)
-        # unknown option
-        ;;
+            # unknown option
+            echo "[Error] Unknow option: $key"
+            exit 1
+            ;;
     esac
     shift # past argument or value
 done
 
 function debug {
-    if [ $DEBUG=YES ]; then
-        echo $1
+    if [ $DEBUG ]; then
+        echo "[Debug] $1"
     fi
 }
 
@@ -32,35 +34,35 @@ PROCESS_PATH=processing
 
 if [ $OUTPUT ]; then
     OUTPUT_PATH=$OUTPUT
-    debug 'set output path to' $OUTPUT_PATH
+    debug "set output path to $OUTPUT_PATH"
 else
     OUTPUT_PATH=finished
-    debug 'set output path to' $OUTPUT_PATH
+    debug "set output path to $OUTPUT_PATH"
 fi
 
 if [ ! -e $PROCESS_PATH ]; then
     /bin/mkdir $PROCESS_PATH
-    debug 'mkdir' $PROCESS_PATH
+    debug "mkdir $PROCESS_PATH"
 fi
 
 if [ ! -e $OUTPUT_PATH ]; then
     /bin/mkdir $OUTPUT_PATH
-    debug 'mkdir' $OUTPUT_PATH
+    debug "mkdir $OUTPUT_PATH"
 fi
 
 for i in *.mkv
 do
     /bin/mv "$i" "$PROCESS_PATH/$i"
-    debug 'move' $i 'to' $PROCESS_PATH/$i
+    debug "move $i to $PROCESS_PATH/$i"
     output="${i/mkv/mp4}"
-    debug 'set out name to' $output
+    debug "set out name to $output"
     if [ $SIZE ]; then
         ./ffmpeg -i "$PROCESS_PATH/$i" -vf scale="-2:$SIZE",subtitles="$PROCESS_PATH/$i" "$OUTPUT_PATH/$output"
-        debug 'ffmpeg with size argument'
+        debug "ffmpeg with size argument"
     else
         ./ffmpeg -i "$PROCESS_PATH/$i" -vf subtitles="$PROCESS_PATH/$i" "$OUTPUT_PATH/$output"
-        debug 'ffmpeg default'
+        debug "ffmpeg default"
     fi
     /bin/mv "$PROCESS_PATH/$i" "$OUTPUT_PATH/$i"
-    debug 'move' $PROCESS_PATH/$i 'to' $OUTPUT_PATH/$i
+    debug "move $PROCESS_PATH/$i to $OUTPUT_PATH/$i"
 done
